13c5b044d65b2d30226f2f28a78e44e43fd75567
tejas git <d9489079@gmail.com>
2025-12-09 22:08:36 +0530

    user's related backend been intergrated with redux in frontend

---
+++ frontend/src/store/Users/user-action.js
@@ -0,0 +1,86 @@
+import { axiosInstance } from "../../utils/axios";
+import { userActions } from "./user-slice";
+
+export const setSignup = (user) => async (dispatch) => {
+  try {
+    dispatch(userActions.setSignupRequest());
+    const { data } = await axiosInstance.post("/api/v1/rent/user/signup", user);
+    dispatch(userActions.getSignupDetails(data.user));
+  } catch (error) {
+    dispatch(userActions.getError(error.response.data.message));
+  }
+};
+
+export const getLogin = (user) => async (dispatch) => {
+  try {
+    dispatch(userActions.setLoginRequest());
+    const { data } = await axiosInstance.post("/api/v1/rent/user/login", user);
+
+    dispatch(userActions.getLoginDetails(data.user));
+  } catch (error) {
+    dispatch(userActions.getError(error.response.data.message));
+  }
+};
+
+export const currentUser = () => async (dispatch) => {
+  try {
+    dispatch(userActions.setCurrentUserRequest());
+    const { data } = await axiosInstance.get("/api/v1/rent/user/me");
+    console.log(data);
+    dispatch(userActions.getCurrentUser(data.user));
+  } catch (error) {
+    dispatch(userActions.getError(error.response.data.message));
+  }
+};
+
+export const updatedUser = (updateUser) => async (dispatch) => {
+  try {
+    dispatch(userActions.setUpdatedUserRequest());
+    await axiosInstance.patch("/api/v1/rent/user/updateMe", updateUser);
+    const { data } = await axiosInstance.get("/api/v1/rent/user/me");
+    dispatch(userActions.getCurrentUser(data.user));
+  } catch (error) {
+    dispatch(userActions.getError(error.response.data.message));
+  }
+};
+
+// password
+export const forgotPassword = (email) => async (dispatch) => {
+  try {
+    await axiosInstance.post("/api/v1/rent/user/forgotPassword", { email });
+  } catch (error) {
+    dispatch(userActions.getError(error.response.data.message));
+  }
+};
+
+export const resetPassword = (rePassword, token) => async (dispatch) => {
+  try {
+    await axiosInstance.patch(
+      `/api/v1/rent/user/resetPassword/${token}`,
+      rePassword
+    );
+  } catch (error) {
+    dispatch(userActions.getError(error.response.data.message));
+  }
+};
+
+// update
+export const updatePassword = (passwords) => async (dispatch) => {
+  try {
+    dispatch(userActions.setPasswordRequest());
+    await axiosInstance.patch("/api/v1/rent/user/updateMyPassword", passwords);
+    dispatch(userActions.getPasswordSuccess(true));
+  } catch (error) {
+    dispatch(userActions.getError(error.response.data.message));
+  }
+};
+
+// logout
+export const logout = () => async (dispatch) => {
+  try {
+    await axiosInstance.get("/api/v1/user/logout");
+    dispatch(userActions.getLogout(null));
+  } catch (error) {
+    dispatch(userActions.getError(error.response.data.message));
+  }
+};
---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import qs from "qs";
+
+// serialization means converting a javascript object or array into a URL query String that can e sent in an HTTP request
+
+export const asxiosInstance = axios.create({
+  baseURL: process.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
+});
