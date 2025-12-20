fcc9fe9663ede0d840792fb5dab2d4b3fe3676f8
tejas git <d9489079@gmail.com>
2025-12-20 18:57:37 +0530

    user module is been integrated

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,8 +10,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors({ origin: process.env.ORIGIN_ACCESS_URL, credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
 
 const port = process.env.PORT || 8081;
