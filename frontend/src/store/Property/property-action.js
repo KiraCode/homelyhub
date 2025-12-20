984d6f8edc0cba26904c9f67c0f6d16ada8bf1cd
tejas git <d9489079@gmail.com>
2025-12-20 19:16:18 +0530

    slice and action is been added for properties

---
+++ frontend/src/store/Property/property-action.js
@@ -0,0 +1,22 @@
+import { propertyAction } from "../property-slice.js";
+import { axiosInstance } from "../../utils/axios.js";
+export const getAllProperties = () => async (dispatch, getState) => {
+  try {
+    dispatch(propertyAction.setRequest());
+    const { searchParams } = getState().properties;
+    console.log(searchParams);
+
+    const response = await axiosInstance.get("/api/v1/rent/listing", {
+      params: { ...searchParams },
+    });
+
+    if (!response) {
+      throw new Error("Could not fetch any properties");
+    }
+
+    const { data } = response;
+    dispatch(propertyAction.getProperties(data));
+  } catch (error) {
+    dispatch(propertyAction.getErrors(error.message));
+  }
+};
