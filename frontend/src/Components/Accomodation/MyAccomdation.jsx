b16006a8d6666a853bf9d006dd2c6f9d6908eae1
tejas git <d9489079@gmail.com>
2025-12-28 10:05:45 +0530

    accomodation is completed

--- frontend/src/Components/Accomodation/MyAccomdation.jsx
+++ frontend/src/Components/Accomodation/MyAccomdation.jsx
@@ -1,45 +1,49 @@
 import React from "react";
 
-const MyAccomodation = () => {
+const MyAccomodation = ({ accomodation }) => {
   return (
     <div className="main-container">
-      <div className="myaccomodation-container row">
-        <div className="myaccomodation-image-container col-lg-3 col-md-3">
-          <img
-            className="myaccomodation-img"
-            src="../assets/image1.jpeg"
-            alt="myaccomdation"
-          />
-        </div>
-        <div className="myaccomodation-information col-lg-9 col-md-9">
-          <h6 className="myaccomodation-hotel-name">
-            Sea view cottages & complementry meals included
-          </h6>
-          <div className="stay-information">
-            <span className="info">
-              <span className="material-symbols-outlined icon">
-                calendar_month
-              </span>
-              2024-01-22
-            </span>
-            <span className="material-symbols-outlined icon">
-              arrow_forward
-            </span>
-            <span className="info">
-              <span className="material-symbols-outlined icon">
-                calendar_month
-              </span>
-              2024-01-24
-            </span>
+      {accomodation.map((accomodation) => (
+        <div className="myaccomodation-container row">
+          <div className="myaccomodation-image-container col-lg-3 col-md-3">
+            <img
+              className="myaccomodation-img"
+              src={accomodation.images[0].url}
+              alt={accomodation.propertyName}
+            />
           </div>
-          <p className="myaccomodation-city">City :Bangalore</p>
-          <p className="myaccomodation-guest">Max no of guest : 2</p>
-          <h5 className="myaccomodation-price">
-            <span className="material-symbols-outlined">payments</span> Total
-            Price :&#8377; 13999
-          </h5>
+          <div className="myaccomodation-information col-lg-9 col-md-9">
+            <h6 className="myaccomodation-hotel-name">
+              {accomodation.propertyName}
+            </h6>
+            <div className="stay-information">
+              <span className="info">
+                <span className="material-symbols-outlined icon">
+                  calendar_month
+                </span>
+                Check In:{accomodation.checkInTime}
+              </span>
+              <span className="material-symbols-outlined icon">
+                arrow_forward
+              </span>
+              <span className="info">
+                <span className="material-symbols-outlined icon">
+                  calendar_month
+                </span>
+                Check Out:{accomodation.checkOutTime}
+              </span>
+            </div>
+            <p className="myaccomodation-city">City :Bangalore</p>
+            <p className="myaccomodation-guest">
+              Max no of guest : {accomodation.maximumGuest}
+            </p>
+            <h5 className="myaccomodation-price">
+              <span className="material-symbols-outlined">payments</span> Total
+              Price :&#8377; 13999
+            </h5>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
